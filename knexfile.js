9a27c3752d6dd38bd9f0edaf52117ecf0bbc18cb
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-22 03:57:08 +0000

    creation of migration file knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      user: 'development',
+      password: 'development',
+      database: "test_db"
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
